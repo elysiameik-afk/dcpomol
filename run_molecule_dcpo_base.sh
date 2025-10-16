32d5f918322e81ed97357b711e127823a79372f6
elysiameik-afk <elysiameik@gmail.com>
2025-10-16 09:59:23 +0800

    Update run_molecule_dcpo_base.sh

--- run_molecule_dcpo_base.sh
+++ run_molecule_dcpo_base.sh
@@ -210,7 +210,7 @@ if [ ${RANK} == 0 ]; then
     actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=${infer_ppo_max_token_len} \
     actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=${infer_ppo_max_token_len} \
     actor_rollout_ref.model.path="${MODEL_PATH}" \
-    actor_rollout_ref.model.tokenizer_path="${TOKENIZER_PATH}" \
+    +actor_rollout_ref.model.tokenizer_path="${TOKENIZER_PATH}" \
     +actor_rollout_ref.model.override_config.attention_dropout=0. \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.optim.lr=1e-6 \
